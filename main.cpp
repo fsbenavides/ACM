ba04972a0480dfe706c1b3b1a73a50c552a5c0fa
fsbenavides <31104984+fsbenavides@users.noreply.github.com>
2018-03-24 15:20:09 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,40 +6,48 @@ using namespace std;
 int main () {
 	
 	char x[4];
+	x='hola';
 	int xlen=4;
 	int z[xlen];
 	int z1=0;
-	char alfabeto[25];
-
+	char alfabeto[27];
+	char alfabetoin[27];
 	
-	/*
-	alfabeto[0}="a";
-	alfabeto[1]="b";
-	alfabeto[2]="c";
-	alfabeto[3]="d";
-	alfabeto[4]="e";
-	alfabeto[5]="f";
-	alfabeto[6]="g";
-	alfabeto[7]="h";
-	*/
 	
-	for(int i=0;i<25;i++){
+	for(int i=0;i<27;i++){
 		int a=97;
 		alfabeto[i]=a;
 		a++;
 	}
 	
+	for(int i=0;i<27;i++;){
+		int a=122;
+		alfabetoin[i]=a;
+		a--;
+	}
+	
 	
 	for(int i=0;i<xlen;i++){
-		for(int j=0;j<25;j++){
+		for(int j=0;j<27;j++){
 			
 			if(x[i]==alfabeto[j]){
 				z[z1]=j;
 				z1++;
-				
 			}
-			   
 		}
+	}
+	
+	for(int i=0;i<z1;i++){
+		for(int j=0;j<27;j++){
+			
+			if (z[i]==j){
+				x[i]==alfabetoin[j];
+			}
+			
+		}
+		
+	
+		
 		
 	}
 	
